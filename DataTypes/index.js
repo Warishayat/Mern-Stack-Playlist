fd5b3608e4a48d249262e51f651310c38b135ff9
waris hayat <warishayat666@gmail.com>
2025-05-30 00:51:04 +0500

    basic Data types and var

---
+++ DataTypes/index.js
@@ -0,0 +1,50 @@
+// var age = 25;
+// if (true){
+    
+//     console.log(age);
+//     }
+
+// function scope
+// function solve(){
+//     var age=25;
+// }
+// console.log(age)
+// solve()
+
+// let a =10;
+// console.log(a);
+
+
+// let didnt allow re declarration
+
+// {
+//     let b = 10;
+//     // it allow reassigning but didnt allow re initailization
+//     b=100;             
+//     console.log(b);
+// }
+
+// const a = 12;
+// // a=100;
+// // const a = 100;
+// console.log(a);
+
+
+// number
+let num=12;
+console.log(num);
+
+// string
+let name = "waris" ;
+console.log("The name is :",name);
+
+// undefined
+let school;
+console.log("The school name is:",school);
+
+// boolean
+let present = false;
+console.log(present);
+
+// to check the type of the variable
+console.log(typeof(present));
